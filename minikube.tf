9408ad95a3427093d7bc39e4d27740151abf52cb
PramodCodes <147572924+PramodCodes@users.noreply.github.com>
2025-01-20 16:27:02 +0530

    update

--- minikube.tf
+++ minikube.tf
@@ -1,12 +1,15 @@
 module "minikube" {
-  source = "github.com/PramodCodes/terraform-aws-minikube-module"
+#   source = "github.com/scholzj/terraform-aws-minikube"
+source = "github.com/PramodCodes/terraform-aws-minikube-module"
+
 
   aws_region    = "us-east-1"
   cluster_name  = "roboshop"
   aws_instance_type = "t3.medium"
   ssh_public_key = "~/.ssh/id_rsa.pub" #get your public key from /users/username/.ssh/ and use here
   aws_subnet_id = "subnet-08552b8a3fc9570b4" #replace with your default subnet id
-  ami_image_id = "ami-0b4f379183e5706b9" # this is built on centos 7 but i am trying with 8
+#   ami_image_id = "ami-0b4f379183e5706b9" # this is built on centos 7 but i am trying with 8
+#   ami_image_id = data.aws_ami.Centos8.id
   hosted_zone = "pka.in.net"
   hosted_zone_private = false
 
